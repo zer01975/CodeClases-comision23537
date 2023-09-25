2d14fce91c10f4b607678545e95611dd8c594e1d
MarceloPesoa <97689731+zer01975@users.noreply.github.com>
2023-09-25 10:21:38 -0300

    spaabse key

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
-var SUPABASE_URL = 'https://dxbvxrzjngnsfetmqite.supabase.co'
+var SUPABASE_URL = 'https://jfziwaejkmzukwgwwxda.supabase.co'
 var SUPABASE_KEY =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR4YnZ4cnpqbmduc2ZldG1xaXRlIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTU0ODIzODEsImV4cCI6MjAxMTA1ODM4MX0.shFIYIVqyEvNBMFbxBjAdSmCHKoWw3yF-ADnAdPks4c'
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Impmeml3YWVqa216dWt3Z3d3eGRhIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTU0OTA1MzIsImV4cCI6MjAxMTA2NjUzMn0.jqG4efSGLveVIpXkFujFPXN9WmvdEp813OI4HfL4GgI'
 
 var supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY)
 window.userToken = null
