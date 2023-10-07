e9b055ded5bf7a5c910ed2845d21ab67a2736271
MarceloPesoa <97689731+zer01975@users.noreply.github.com>
2023-10-07 02:02:26 -0300

    footer Js innerHTML

---
+++ js/login.js
@@ -0,0 +1,69 @@
+var SUPABASE_URL = 'https://jfziwaejkmzukwgwwxda.supabase.co'
+var SUPABASE_KEY =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Impmeml3YWVqa216dWt3Z3d3eGRhIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTU0OTA1MzIsImV4cCI6MjAxMTA2NjUzMn0.jqG4efSGLveVIpXkFujFPXN9WmvdEp813OI4HfL4GgI'
+
+
+var supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY)
+window.userToken = null
+
+document.addEventListener('DOMContentLoaded', function (event) {
+ 
+  var logInForm = document.querySelector('#log-in')
+  logInForm.onsubmit = logInSubmitted.bind(logInForm)
+  
+})
+
+
+const logInSubmitted = (event) => {
+  event.preventDefault()
+  const email = event.target[0].value
+  const password = event.target[1].value
+
+  supabase.auth
+    .signIn({ email, password })
+    .then((response) => {
+     //response.error ? alert(response.error.message) : setToken(response)
+      response.error ? alert("datos incorrectos") : setToken(response)
+    })
+    .catch((err) => {
+      //alert("datos incorrectos")
+      alert(err.response.text)
+    })
+}
+
+const fetchUserDetails = () => {
+  alert(JSON.stringify(supabase.auth.user()))
+}
+
+const logoutSubmitted = (event) => {
+  event.preventDefault()
+
+  supabase.auth
+    .signOut()
+    .then((_response) => {
+      document.querySelector('#access-token').value = ''
+      document.querySelector('#refresh-token').value = ''
+    alert('Logout successful')
+      
+    })
+    .catch((err) => {
+      //alert("datos incorrectos")
+      alert(err.response.text)
+    })
+}
+
+function setToken(response) {
+  if (response.user.confirmation_sent_at && !response?.session?.access_token) {
+    alert('Confirmation Email Sent')
+  } else {
+    document.querySelector('#access-token').value = response.session.access_token
+    document.querySelector('#refresh-token').value = response.session.refresh_token
+    alert('Bienvenido ' + response.user.email)
+    document.getElementById("log-in").reset();
+    window.location="prueba.html";
+    
+    
+  }
+  
+}
+
--- js/main.js
+++ js/main.js
@@ -1,69 +1,33 @@
-var SUPABASE_URL = 'https://jfziwaejkmzukwgwwxda.supabase.co'
-var SUPABASE_KEY =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Impmeml3YWVqa216dWt3Z3d3eGRhIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTU0OTA1MzIsImV4cCI6MjAxMTA2NjUzMn0.jqG4efSGLveVIpXkFujFPXN9WmvdEp813OI4HfL4GgI'
 
-
-var supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY)
-window.userToken = null
-
-document.addEventListener('DOMContentLoaded', function (event) {
- 
-  var logInForm = document.querySelector('#log-in')
-  logInForm.onsubmit = logInSubmitted.bind(logInForm)
-  
-})
-
-
-const logInSubmitted = (event) => {
-  event.preventDefault()
-  const email = event.target[0].value
-  const password = event.target[1].value
-
-  supabase.auth
-    .signIn({ email, password })
-    .then((response) => {
-     //response.error ? alert(response.error.message) : setToken(response)
-      response.error ? alert("datos incorrectos") : setToken(response)
-    })
-    .catch((err) => {
-      //alert("datos incorrectos")
-      alert(err.response.text)
-    })
-}
-
-const fetchUserDetails = () => {
-  alert(JSON.stringify(supabase.auth.user()))
-}
-
-const logoutSubmitted = (event) => {
-  event.preventDefault()
-
-  supabase.auth
-    .signOut()
-    .then((_response) => {
-      document.querySelector('#access-token').value = ''
-      document.querySelector('#refresh-token').value = ''
-    alert('Logout successful')
-      
-    })
-    .catch((err) => {
-      //alert("datos incorrectos")
-      alert(err.response.text)
-    })
-}
-
-function setToken(response) {
-  if (response.user.confirmation_sent_at && !response?.session?.access_token) {
-    alert('Confirmation Email Sent')
-  } else {
-    document.querySelector('#access-token').value = response.session.access_token
-    document.querySelector('#refresh-token').value = response.session.refresh_token
-    alert('Bienvenido ' + response.user.email)
-    document.getElementById("log-in").reset();
-    window.location="prueba.html";
-    
-    
-  }
-  
-}
-
+let pie = `
+    <div class="grupo-1">
+                    <div class="logo">
+                        <a href="#">
+                            <img src="img/CodoACodo.jpg" alt="Logo de SLee Dw">
+                        </a>
+                    </div>
+                    <div class="box">
+                        <h2>Nombre Proyecto</h2>
+                        <ul>
+                            <li><a href="#">Sobre el Proyecto</a></li>
+                            <li><a href="#">Contacto</a></li>
+                            <li><a href="#">Preguntas frecuentes</a></li>
+                            <li><a href="#">Ayuda</a></li>
+                        </ul>
+                    </div>
+                    <div class="box">
+                        <h2>SIGUENOS</h2>
+                        <div class="red-social">
+                            <a href="https://es-la.facebook.com/" class="fa fa-facebook"></a>
+                            <a href="https://www.instagram.com/" class="fa fa-instagram"></a>
+                            <a href="https://twitter.com/?lang=es" class="fa fa-twitter"></a>
+                            <a href="https://www.youtube.com/" class="fa fa-youtube"></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="grupo-2">
+                    <p>&copy; 2023 <b>Clase-Codo a Codo</b> - Sitio desarrollado por:</p>
+                    <span>Lucas González, Marcelo Pesoa, Lisandro Iguacel y Marcilia Nietto</span>
+                </div>
+    `
+document.querySelector(".pie-pagina").innerHTML = pie
