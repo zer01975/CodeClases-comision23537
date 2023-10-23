faa43f13938fa6f27a7b78e8bad22d595f3f7788
lucasgonzalez939 <lucasgonzalez939@gmail.com>
2023-10-23 13:16:43 -0300

    Cambios en varios archivos para obtener los datos de cursos

--- js/main-logged.js
+++ js/main-logged.js
@@ -14,33 +14,16 @@ let pie = `
     <div class="grupo-1">
                     <div class="logo">
                         <a href="#">
-                            <img src="img/CodoACodo.jpg" alt="Logo de SLee Dw">
+                            <img class="logo" src="img/CodoACodo.jpg" alt="Logo de SLee Dw">
                         </a>
                     </div>
-                    <div class="box">
-                        <h2>Nombre Proyecto</h2>
-                        <ul>
-                            <li><a href="#">Sobre el Proyecto</a></li>
-                            <li><a href="#">Contacto</a></li>
-                            <li><a href="#">Preguntas frecuentes</a></li>
-                            <li><a href="#">Ayuda</a></li>
-                        </ul>
-                    </div>
-                    <div class="box">
-                        <h2>SIGUENOS</h2>
-                        <div class="red-social">
-                            <a href="https://es-la.facebook.com/" class="fa fa-facebook"></a>
-                            <a href="https://www.instagram.com/" class="fa fa-instagram"></a>
-                            <a href="https://twitter.com/?lang=es" class="fa fa-twitter"></a>
-                            <a href="https://www.youtube.com/" class="fa fa-youtube"></a>
-                        </div>
-                    </div>
-                </div>
+                   
                 <div class="grupo-2">
                     <p>&copy; 2023 <b>Clase-Codo a Codo</b> - Sitio desarrollado por:</p>
                     <span>Lucas González, Marcelo Pesoa, Lisandro Iguacel y Marcilia Nietto</span>
                 </div>
+    </div>
     `
 
 document.querySelector(".nav-menu").innerHTML = nav
-document.querySelector(".pie-pagina").innerHTML = pie
+document.querySelector("#footer").innerHTML = pie
