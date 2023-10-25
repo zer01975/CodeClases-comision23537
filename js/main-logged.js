0111e78626e9b14b99b0f57fe4840a4bbdfe0f25
lucasgonzalez939 <lucasgonzalez939@gmail.com>
2023-10-25 18:07:55 -0300

    correcciones en dashboard, cursos y estudiantes

--- js/main-logged.js
+++ js/main-logged.js
@@ -12,25 +12,6 @@ let nav = `<nav class="nav-menu">
 
 let pie = `
     <div class="grupo-1">
-                    <div class="logo">
-                        <a href="#">
-                        <img src="./img/Copia de Circuit Technology Logo (4).png" alt="Logo del sitio">
-                        </a>
-                    </div>
-                    <div class="box">
-                        <h2>MIS CLASES</h2>
-                       
-                    </div>
-                    <div class="box">
-                        <h2>SIGUENOS</h2>
-                        <div class="red-social">
-                            <a href="https://es-la.facebook.com/" class="fa fa-facebook"></a>
-                            <a href="https://www.instagram.com/" class="fa fa-instagram"></a>
-                            <a href="https://twitter.com/?lang=es" class="fa fa-twitter"></a>
-                            <a href="https://www.youtube.com/" class="fa fa-youtube"></a>
-                        </div>
-                    </div>
-                </div>
                 <div class="grupo-2">
                     <p>&copy; 2023 <b>Clase-Codo a Codo</b> - Sitio desarrollado por:</p>
                     <span>Lucas González, Marcelo Pesoa, Lisandro Iguacel y Marcilia Nietto</span>
@@ -39,4 +20,4 @@ let pie = `
     `
 
 document.querySelector(".nav-menu").innerHTML = nav
-document.querySelector("#footer").innerHTML = pie
+document.querySelector(".pie-pagina").innerHTML = pie
