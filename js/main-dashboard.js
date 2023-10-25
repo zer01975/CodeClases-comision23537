d7befc219d93519ba363fb0b13aab877950809a6
lucasgonzalez939 <lucasgonzalez939@gmail.com>
2023-10-25 19:13:26 -0300

    Muestra un curso y sus alumnos en el Dashboard

--- js/main-dashboard.js
+++ js/main-dashboard.js
@@ -32,7 +32,7 @@ async function fetchCourseId() {
             console.error('Error recibiendo el id del curso:', error);
             return null;
         }
-        return JSON.stringify(cursos);
+        return cursos[0].course_id;
 
     } catch (err) {
         console.error('Error en fetchCourseId:', err)
@@ -40,93 +40,98 @@ async function fetchCourseId() {
     }
 }
 
-courseId = fetchCourseId();
+document.addEventListener('DOMContentLoaded', async function () {
+    try {
+        const courseId = await fetchCourseId();
 
-document.addEventListener('DOMContentLoaded', function () {
-    async function displayCourses() {
-        try {
-            let { data: cursos, error } = await supabase
-                .from('cursos')
-                .select('*') // Seleccionar todas las columnas
-                .eq('course_id', courseId);
-
-            if (error) {
-                console.error('Error recibiendo datos de curso:', error);
-                return;
-            }
-
-            const coursesContainer = document.getElementById('coursesContainer');
-
-            cursos.forEach((curso) => {
-                // Crear un div para cada curso
-                const card = document.createElement('div');
-                card.className = 'course-card';
-
-                // Crear elementos para cada dato de la fila
-                const courseName = document.createElement('h2');
-                courseName.textContent = curso.course_name;
-
-                const courseDescription = document.createElement('p');
-                courseDescription.textContent = curso.course_info;
-
-                //Se pueden agregar mas campos
-
-                // Agregar los elementos a la tarjeta (el div)
-                card.appendChild(courseName);
-                card.appendChild(courseDescription);
-
-                // Agregar la tarjeta al contenedor
-                coursesContainer.appendChild(card);
-            });
-        } catch (err) {
-            console.error('Error en displayCourses:', err);
+        if (!courseId) {
+            console.error('ID de curso no encontrado.');
+            return;
         }
+
+        await displayCourses(courseId);
+        await displayStudents(courseId);
+    } catch (err) {
+        console.error('Error en main:', err);
     }
-})
+});
 
+async function displayCourses(courseId) {
+    try {
+        let { data: cursos, error } = await supabase
+            .from('cursos')
+            .select('*') // Seleccionar todas las columnas
+            .eq('course_id', [courseId]);
 
-document.addEventListener('DOMContentLoaded', function () {
-    async function displayStudents() {
-        try {
-            let { data: estudiantes, error } = await supabase
-                .from('estudiantes')
-                .select('*') // Seleccionar todas las columnas
-                .eq('course_id', courseId);
-
-            if (error) {
-                console.error('Error recibiendo datos de estudiantes:', error);
-                return;
-            }
-
-            const studentsContainer = document.getElementById('studentsContainer');
-
-            estudiantes.forEach((estudiante) => {
-                // Crear un div para cada curso
-                const card = document.createElement('div');
-                card.className = 'student-card';
-
-                // Crear elementos para cada dato de la fila
-                const studentName = document.createElement('h2');
-                studentName.textContent = estudiante.student_name + ' ' + estudiante.student_lastname;
-
-                const studentDescription = document.createElement('p');
-                studentDescription.textContent = estudiante.student_description;
-
-                //Se pueden agregar mas campos
-
-                // Agregar los elementos a la tarjeta (el div)
-                card.appendChild(studentName);
-                card.appendChild(studentDescription);
-
-                // Agregar la tarjeta al contenedor
-                studentsContainer.appendChild(card);
-            });
-        } catch (err) {
-            console.error('Error en displayStudents:', err);
+        if (error) {
+            console.error('Error recibiendo datos de curso:', error);
+            return;
         }
+
+        const coursesContainer = document.getElementById('coursesContainer');
+
+        cursos.forEach((curso) => {
+            // Crear un div para cada curso
+            const card = document.createElement('div');
+            card.className = 'course-card';
+
+            // Crear elementos para cada dato de la fila
+            const courseName = document.createElement('h2');
+            courseName.textContent = curso.course_name;
+
+            const courseDescription = document.createElement('p');
+            courseDescription.textContent = curso.course_info;
+
+            //Se pueden agregar mas campos
+
+            // Agregar los elementos a la tarjeta (el div)
+            card.appendChild(courseName);
+            card.appendChild(courseDescription);
+
+            // Agregar la tarjeta al contenedor
+            coursesContainer.appendChild(card);
+        });
+    } catch (err) {
+        console.error('Error en displayCourses:', err);
     }
-})
+}
 
-displayCourses();
+async function displayStudents(courseId) {
+    try {
+        let { data: estudiantes, error } = await supabase
+            .from('estudiantes')
+            .select('*') // Seleccionar todas las columnas
+            .contains('course_id', [courseId]);
 
-displayStudents();
+        if (error) {
+            console.error('Error recibiendo datos de estudiantes:', error);
+            return;
+        }
+
+        const studentsContainer = document.getElementById('studentsContainer');
+
+        estudiantes.forEach((estudiante) => {
+            // Crear un div para cada curso
+            const card = document.createElement('div');
+            card.className = 'student-card';
+
+            // Crear elementos para cada dato de la fila
+            const studentName = document.createElement('h2');
+            studentName.textContent = estudiante.student_name + ' ' + estudiante.student_lastname;
+
+            const studentDescription = document.createElement('p');
+            studentDescription.textContent = estudiante.student_description;
+
+            //Se pueden agregar mas campos
+
+            // Agregar los elementos a la tarjeta (el div)
+            card.appendChild(studentName);
+            card.appendChild(studentDescription);
+
+            // Agregar la tarjeta al contenedor
+            studentsContainer.appendChild(card);
+        });
+    } catch (err) {
+        console.error('Error en displayStudents:', err);
+    }
+}
